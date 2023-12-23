91357d28a7f2adcf2e166e93e2de3fa7aecca240
Hakan Turan <chakanturan@icloud.com>
2023-12-23 14:12:10 +0300

    repo ilk ekleme.

---
+++ src/Giris/Baslangic.java
@@ -0,0 +1,17 @@
+package Giris;
+
+public class Baslangic {
+    public static void main(String[] args) {
+
+       /* System.out.println("Merhaba Dünya !!");
+        System.out.println("sa as");
+        burası gelir gider */
+
+        int kisaKenar = 10, uzunKenar = 20;
+        int alan = kisaKenar*uzunKenar;
+        int cevre = 2*(kisaKenar+uzunKenar);
+        System.out.println(alan);
+        System.out.println(cevre);
+
+    }
+}
